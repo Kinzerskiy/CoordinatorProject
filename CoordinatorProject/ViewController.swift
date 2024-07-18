0c5ee23b04b31e0945a2be2c2c8e26eb7e5fb90d
Anton <kinzzersky@gmail.com>
2024-07-18 13:13:39 +0300

    Initial Commit

---
+++ CoordinatorProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorProject
+//
+//  Created by Anton on 18.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
